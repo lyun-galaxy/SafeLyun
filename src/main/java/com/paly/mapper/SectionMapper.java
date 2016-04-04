7ced84e0e64e8c32b5ee26a099b069a5f6c50cd0
dengfengdecao <dengfengdecao@gmail.com>
2016-04-04 22:39:44 +0800

    完成Section和Subsection的关系映射并完善接口文档

--- src/main/java/com/paly/mapper/SectionMapper.java
+++ src/main/java/com/paly/mapper/SectionMapper.java
@@ -2,5 +2,17 @@ package com.paly.mapper;
 
 import com.paly.domain.Section;
 
+/**
+ * 章节Dao接口
+ * @author linyu
+ *
+ */
 public interface SectionMapper extends BaseMapper<Section> {   
+	
+	/**
+	 * 根据子章节id获取其所属的章节信息
+	 * @param subsectionId 子章节id
+	 * @return 返回指定子章节所属的章节信息
+	 */
+	Section getBySubsectionId(int subsectionId);
 }
--- src/main/java/com/paly/mapper/SubsectionMapper.java
+++ src/main/java/com/paly/mapper/SubsectionMapper.java
@@ -1,5 +1,7 @@
 package com.paly.mapper;
 
+import java.util.List;
+
 import com.paly.domain.Subsection;
 
 /**
@@ -9,4 +11,10 @@ import com.paly.domain.Subsection;
  */
 public interface SubsectionMapper extends BaseMapper<Subsection> {
    
+	/**
+	 * 根据章节Id获取其下的子章节列表
+	 * @param sectionId 章节id
+	 * @return 返回指定章节下的子章节列表
+	 */
+	List<Subsection> getBySectionId(int sectionId);
 }
