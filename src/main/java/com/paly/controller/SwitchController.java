1df45975d2c0afad51928256c2e104b41301f21e
ron <22713560@qq.com>
2016-04-16 14:54:44 +0800

    修复考试开关BUG

---
+++ src/main/java/com/paly/controller/SwitchController.java
@@ -0,0 +1,53 @@
+package com.paly.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.paly.domain.Examswitch;
+import com.paly.mapper.ExamswitchMapper;
+import com.paly.service.AdminExamSwichService;
+import com.paly.vo.Json;
+
+@Controller
+public class SwitchController extends BaseController{
+
+	@Resource
+	AdminExamSwichService adminExamSwichService;
+	
+	@RequestMapping("/switchController/changeFlag.action")
+	public void changeFlag(HttpServletResponse response){
+		Json json = new Json();
+		try {
+			Boolean flag = adminExamSwichService.getFlag();
+			adminExamSwichService.changFlag(flag);
+			json.setSuccess(true);
+			if(flag){
+			    json.setMsg("关闭考试开关！");
+			}else{
+				json.setMsg("开启考试开关！");
+			}
+		} catch (Exception e) {
+			json.setMsg(e.getMessage());
+		}
+		super.writeJson(json,response);
+	}
+	
+	@RequestMapping("/switchController/getFlag.action")
+	public void getFlag(HttpServletResponse response){
+		Json json = new Json();
+		try {
+			//adminExamSwichService.changFlag();
+			Boolean flag = adminExamSwichService.getFlag();	
+	        json.setFlag(flag);
+			json.setSuccess(true);
+			json.setMsg("修改成功！");
+		} catch (Exception e) {
+			json.setMsg(e.getMessage());
+		}
+		super.writeJson(json,response);
+	}
+	
+}
---
+++ src/main/java/com/paly/service/AdminExamSwichService.java
@@ -0,0 +1,7 @@
+package com.paly.service;
+
+public interface AdminExamSwichService {
+   public void changFlag(Boolean flag);
+   
+   public boolean getFlag();
+}
