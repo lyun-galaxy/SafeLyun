aa4f8c1c96d661d4da1945836598630fd528153e
ron <22713560@qq.com>
2016-04-24 19:31:25 +0800

    合并工作流

--- src/main/java/com/paly/controller/AdminUserController.java
+++ src/main/java/com/paly/controller/AdminUserController.java
@@ -43,7 +43,7 @@ public class AdminUserController extends AdminBaseController{
 			json.setSuccess(true);
 			json.setMsg("导入成功！");
 		} catch (Exception e) {
-			json.setMsg("导入失败！请勿导入重复的学生信息，请检查文件后再进行导入");
+			json.setMsg(e.toString());
 		}
 		
 	    super.writeJson(json,response);
--- src/main/java/com/paly/controller/client/LoginController.java
+++ src/main/java/com/paly/controller/client/LoginController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -27,23 +28,22 @@ public class LoginController {
 
 	@RequestMapping("/login")
 	@ResponseBody
-	public Map<String,Object> login(HttpSession session, User user) {
-		// User u = userService.findByNumberAndNameAndPassword(user);
-		System.out.println(user.getUserName()+"  "+user.getUserPassword());
-		Map<String,Object> map = new HashMap<String,Object>();
+	public Map<String, Object> login(HttpSession session, User user) {
+		Map<String, Object> map = new HashMap<String, Object>();
 		User u = userService.getByUsernameAndPassword(user.getUserName(), user.getUserPassword());
 		if (u == null) {
-			map.put("check",false);
+			map.put("check", false);
 		} else {
-			map.put("check",true);
+			map.put("check", true);
 			map.put("url", "client_login/toHomePage.action");
 			session.setAttribute("user", u);
 		}
 		return map;
 	}
-	
+
 	/**
 	 * 跳转到主界面
+	 * 
 	 * @return
 	 */
 	@RequestMapping("toHomePage")
@@ -52,8 +52,8 @@ public class LoginController {
 	}
 
 	@RequestMapping("logout")
-	public String logout(HttpSession session) {
+	public String logout(HttpSession session, HttpServletRequest request) {
 		session.invalidate();
-		return "../../login.html";
+		return "redirect:/login.html";
 	}
 }
