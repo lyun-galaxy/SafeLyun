13198902fb88831034f1fa844dc52378c879e51f
ron <22713560@qq.com>
2016-04-26 17:32:38 +0800

    修改sectionMapper中模糊查找的sql语句

---
+++ src/main/java/com/paly/vo/SectionQueryVo.java
@@ -0,0 +1,18 @@
+package com.paly.vo;
+
+public class SectionQueryVo {
+    private int sectionId;
+    private String sectionName;
+	public int getSectionId() {
+		return sectionId;
+	}
+	public void setSectionId(int sectionId) {
+		this.sectionId = sectionId;
+	}
+	public String getSectionName() {
+		return sectionName;
+	}
+	public void setSectionName(String sectionName) {
+		this.sectionName = sectionName;
+	}
+}
